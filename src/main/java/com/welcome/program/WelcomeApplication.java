2c17a5fc2890cf5e16b0785215b62d8c82b329b7
Logadheep <logadheep3438@gmail.com>
2023-02-22 09:55:55 +0530

    Update WelcomeApplication.java

--- src/main/java/com/welcome/program/WelcomeApplication.java
+++ src/main/java/com/welcome/program/WelcomeApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WelcomeApplication {
 	
 	public static void main(String[] args) {
-		WebApplication.run();
+		SpringApplication.run(WelcomeApplication.class, args);
 	}
 
 }
